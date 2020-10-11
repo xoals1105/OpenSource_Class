079de39b1a4f7043dcd5717b852c9d3e249e7dcf
xoals1105 <world32033@gmail.com>
2020-10-11 14:31:34 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 def findMax(a, b, c):
         if a>b:
-                biggest=a 
+                big=a 
         else:
-                biggest=b
+                big=b
         if c>biggest:
-                biggest=c
+                big=c
 
-        return biggest
+        return big
 
 a = int(input("첫 번째 숫자 입력:"))
 b = int(input("두 번째 숫자 입력:"))
