839caeada7330e0e217752131aac762842c2c8e3
xoals1105 <world32033@gmail.com>
2020-10-11 15:56:36 +0900

    Update test.py
    findMax함수 내 변수명 변경(biggest -> big)

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 def findMax(a, b, c):
         if a>b:
-                biggest=a 
+                big=a 
         else:
-                biggest=b
+                big=b
                 
-        if c>biggest:
-                biggest=c
+        if c>big:
+                big=c
 
-        return biggest
+        return big
 
 a = int(input("첫 번째 숫자 입력:"))
 b = int(input("두 번째 숫자 입력:"))
